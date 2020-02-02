1108aec739853800954f4078ecaa5025d1cc77f1
Manas-Kashyap <manaskashyaptech@gmail.com>
2020-02-02 20:15:07 +0530

    user defined choices added

--- src/main/scala/com/knoldus/Person.scala
+++ src/main/scala/com/knoldus/Person.scala
@@ -1,20 +1,33 @@
 package com.knoldus
 
-class Person(val name: String, val age: Int) extends Ordered[Person] {
-  def compare(that: Person): Int = {
-    if (this.name == that.name) {
+class Person(val name:String,val age:Int) extends Ordered[Person]{
+  def compare(that:Person): Int= {
+    if(this.name == that.name){
       this.age - that.age
     }
-    else {
-      this.name.length - that.name.length
-    }
+    else this.name.length - that.name.length
   }
+
 }
 
-object Person extends App {
+object Person extends App{
+  val firstName = scala.io.StdIn.readLine("Enter first name")
+  println("Enter first age")
+  val firstAge = scala.io.StdIn.readInt()
+  val firstPerson:Person = new Person(firstName,firstAge)
+  val secondName = scala.io.StdIn.readLine("Enter second name")
+  println("Enter second age")
+  val secondAge = scala.io.StdIn.readInt()
+  val secondPerson: Person = new Person(secondName,secondAge)
+  println("What you want to do \n" +
+  "1. firstPerson < secondPerson \n" +
+  "2. firstPerson > secondPerson \n")
+  val choice:Int = scala.io.StdIn.readInt()
+  val check = choice match{
+    case 1 => firstPerson < secondPerson
+    case 2 => firstPerson > secondPerson
+    case _ => println("Can't compare")
+  }
 
-  val personOne = new Person("test", 30)
-  val personTwo = new Person("testcase", 39)
-  val check = personOne < personTwo
-  print(check)
+  println(check)
 }
